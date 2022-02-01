d831b3befdbea26431a28f58b1b4276f9921c2f5
XNycX <nycx2009@hotmail.com>
2022-02-01 22:08:06 +0100

    Select character funcionality added

--- js/main.js
+++ js/main.js
@@ -14,3 +14,29 @@ const cambiaPantalla = (cambio) => {
     }
 
 }
+
+const selectKart = (numeroCoche) => {
+
+    if(equipo1 == "") {
+        equipo1 = allCars[numeroCoche];
+        let cochePrimero = document.getElementById(numeroCoche)
+        //una vez he escogido el coche, invalido el img para que nadie haga onclick sobre él
+        
+        cochePrimero.disabled = true;  
+        cochePrimero.classList.add("kartSelected")
+        
+    } else if (equipo2 == "") {
+        equipo2 = allCars[numeroCoche];
+        let cocheSegundo = document.getElementById(numeroCoche)
+        cocheSegundo.disabled = true;
+        cocheSegundo.classList.add("kartSelected")
+
+        //una vez he escogido los dos coches.....
+
+        setTimeout(() => {
+            cambiaPantalla(3);
+        }, 2500)
+    }
+
+
+}
