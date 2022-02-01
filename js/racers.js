45376186f415cd9eaad3e26d409d62e2e38b42e8
XNycX <nycx2009@hotmail.com>
2022-02-01 22:19:58 +0100

    Constructor class of Coche created

---
+++ js/racers.js
@@ -0,0 +1,13 @@
+//desclaracion de la clase
+class Coche {
+
+    constructor(modelo, marca, velocidad, peso, frenada) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.velocidad = velocidad;
+        this.peso = peso;
+        this.frenada = frenada;
+        this.metros = 0;
+
+    }
+};
