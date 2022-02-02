dd2a263ea76c4d43f588f266e48637afc80ada8e
XNycX <nycx2009@hotmail.com>
2022-02-02 20:24:57 +0100

    Function 1 vs cpu added with button  and estela added to characters

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-
-
 const cambiaPantalla = (cambio) => {
 
     let pantallaDeseada = "pantalla" + cambio;
 
-    let arrayPantallas = ["pantalla1","pantalla2","pantalla3","pantalla4"]; //aqui meteriamos todas las pantalla que queramos
+    let arrayPantallas = ["pantalla1", "pantalla2", "pantalla3", "pantalla4"]; //aqui meteriamos todas las pantalla que queramos
 
     arrayPantallas = arrayPantallas.filter(valor => !pantallaDeseada.includes(valor));
 
@@ -19,18 +17,18 @@ const cambiaPantalla = (cambio) => {
 
 const selectKart = (numeroCoche) => {
 
-    if(equipo1 == "") {
+    if (equipo1 == "") {
         equipo1 = allCars[numeroCoche];
         let cochePrimero = document.getElementById(numeroCoche)
         //una vez he escogido el coche, invalido el img para que nadie haga onclick sobre él
-        
-        cochePrimero.onclick = ""   
+
+        cochePrimero.onclick = ""
         cochePrimero.classList.add("kartSelected")
-        
+
     } else if (equipo2 == "") {
         equipo2 = allCars[numeroCoche];
         let cocheSegundo = document.getElementById(numeroCoche)
-        cocheSegundo.onclick = "" 
+        cocheSegundo.onclick = ""
         cocheSegundo.classList.add("kartSelected")
 
         //una vez he escogido los dos coches.....
@@ -38,9 +36,9 @@ const selectKart = (numeroCoche) => {
         setTimeout(() => {
             cambiaPantalla(3);
         }, 2500)
-        
+
         comprobarEquipos();
-        
+
 
     }
 
@@ -52,30 +50,33 @@ let metrosEquipo1 = 0;
 let metrosEquipo2 = 0;
 let metrosGanar = 3000;
 
+const acelerar = () => {
+    metrosEquipo1 += Math.random() * (200 - 50) + 50;
+}
 
 const comprobarEquipos = () => {
-    
-    if ((equipo1 !== "") && (equipo2 !== "")){
-        let metrosRecorridos = window.setInterval(function () {
-            metrosEquipo1 += Math.random() * (200 - 50) + 50;
-            metrosEquipo2 += Math.random() * (200 - 50) + 50;
+    if ((equipo1 !== "") && (equipo2 !== "")) {
+        let metrosRecorridos = window.setInterval(() => {
+            setInterval(() => {
+                metrosEquipo2 += Math.random() * (200 - 50) + 50;
+            }, 1000);
             if (metrosEquipo1 >= metrosGanar || metrosEquipo2 >= metrosGanar) {
                 document.getElementById("botonResultado").style.display = "block";
                 clearInterval(metrosRecorridos);
                 if (metrosEquipo1 > metrosEquipo2) {
+                    document.getElementById('ganador').innerHTML += 'EL GANADOR ES ' + equipo1.nombre.toUpperCase() 
                     document.getElementById("kartGanador").src = "img/" + equipo1.nombre + ".png";
-                }
-                else {
-                    console.log(equipo2)
+                } else {
+                    document.getElementById('ganador').innerHTML += 'EL GANADOR ES' + equipo2.nombre.toUpperCase() 
                     document.getElementById("kartGanador").src = "img/" + equipo2.nombre + ".png";
                 }
             }
             document.getElementById("coche1").src = "img/" + equipo1.nombre + ".png";
             document.getElementById("coche2").src = "img/" + equipo2.nombre + ".png";
-            document.getElementById("contadorCoche1").innerHTML = "Metros recorridos: " + metrosEquipo1.toFixed(2);
-            document.getElementById("contadorCoche2").innerHTML = "Metros recorridos: " + metrosEquipo2.toFixed(2);
+            document.getElementById('btnAcelerar').addEventListener('click', acelerar)
+            document.getElementById("contadorCoche1").innerHTML = "Metros recorridos: " + metrosEquipo1.toFixed(0);
+            document.getElementById("contadorCoche2").innerHTML = "Metros recorridos: " + metrosEquipo2.toFixed(0);
         }, 1000)
-    
     }
 }
 
@@ -85,6 +86,6 @@ const cleanGame = () => {
     metrosEquipo1 = 0;
     metrosEquipo2 = 0;
     cambiaPantalla(1);
-    document.getElementById("botonResultado").style.display = "none"
+    document.getElementById("botonResultado").style.display = "none";
     location.reload();
 }
